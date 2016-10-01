2ba18228b5f1f7544c5e8f75b61461c546a5376f
Ravi <praveshd97@gmail.com>
2016-10-01 12:49:09 +0530

    added even odd program

---
+++ prac.py
@@ -0,0 +1,6 @@
+# program to identify the even/odd state of given number
+number=int(input("enter number: "))
+if number % 2== 0:
+    print("the entered number is even")
+else:
+    print("the entered number is odd")
