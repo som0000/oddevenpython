3ffa8d3c83732d7f350aa3856461b0ff687526c7
annu <annuvishwakarma1998@gmail.com>
2016-10-08 08:58:06 +0530

    Added functions

--- prac.py
+++ prac.py
@@ -1,6 +1,10 @@
 # program to identify the even/odd state of given number
+def isEven(number):
+    if number % 2== 0:
+        print("the entered number is even")
+    else:
+        print("the entered number is odd")
+
+
 number=int(input("enter number: "))
-if number % 2== 0:
-    print("the entered number is even")
-else:
-    print("the entered number is odd")
+isEven(number)
